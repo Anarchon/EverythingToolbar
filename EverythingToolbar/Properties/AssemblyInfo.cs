a928c32597b92575c97809b5b2295dcc5ef8a060
Anarchon <christian.schweinegger@gmail.com>
2020-10-25 14:39:09 +0100

    Add files via upload
    Update Version

--- EverythingToolbar/Properties/AssemblyInfo.cs
+++ EverythingToolbar/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EverythingToolbar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Übersetzung von Anarchon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EverythingToolbar")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.2")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.3.2")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
